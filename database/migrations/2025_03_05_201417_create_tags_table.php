b2be6ca787b3cabbf0f763a4a0e63ed67f7cf798
D254889 <D254889@edu.curio.nl>
2025-03-06 16:42:17 +0100

    Update 2025_03_05_201417_create_tags_table.php

--- database/migrations/2025_03_05_201417_create_tags_table.php
+++ database/migrations/2025_03_05_201417_create_tags_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
             $table->string('tags');
+            $table->string('name');
+            $table->string('color');
             $table->timestamps();
         });
     }
