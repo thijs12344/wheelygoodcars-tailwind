24a88cb975d0edba6c8506c6ee6b150419172691
Femkedelaat <noth4wkss@outlook.com>
2025-03-25 10:02:29 +0100

    edit page update

--- resources/views/Cars/edit.blade.php
+++ resources/views/Cars/edit.blade.php
@@ -3,17 +3,9 @@
 
     <form action="{{ route('cars.update', $car) }}" method="POST">
         @csrf
-        <label for="brand">Merk:</label>
-        <input type="text" name="brand" id="brand" value="{{ $car->brand }}" required>
-
-        <label for="model">Model:</label>
-        <input type="text" name="model" id="model" value="{{ $car->model }}" required>
 
         <label for="price">Prijs:</label>
         <input type="number" step="0.01" name="price" id="price" value="{{ $car->price }}" required>
-
-        <label for="mileage">Kilometerstand:</label>
-        <input type="number" name="mileage" id="mileage" value="{{ $car->mileage }}" required>
 
         <button type="submit">Opslaan</button>
     </form>
