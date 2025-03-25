73c2b73099b1fb3ece19ea7ce11502b007b8ffc8
Femkedelaat <noth4wkss@outlook.com>
2025-03-25 11:05:23 +0100

    Grid ipv table

--- resources/views/Cars/view.blade.php
+++ resources/views/Cars/view.blade.php
@@ -1,38 +1,30 @@
 <x-app-layout>
-    <h1>Mijn auto's</h1>
+    <h1 class="text-4xl font-bold m-4">Mijn auto's</h1>
 
     @if($cars->isEmpty())
         <p>Je hebt nog geen auto's toegevoegd.</p>
     @else
-        <table class="table-fixed">
-            <tr class="border border-black">
-                <th class="th">Kenteken</th>
-                <th class="th">Merk</th>
-                <th class="th">Model</th>
-                <th class="th">Prijs</th>
-                <th class="th">Kilometerstand</th>
-                <th class="th">edit</th>
-                <th class="th">delete</th>
-            </tr>
-            @foreach($cars as $car)
-                <tr>
-                    <td class="td">{{ $car->license_plate }}</td>
-                    <td class="td">{{ $car->brand }}</td>
-                    <td class="td">{{ $car->model }}</td>
-                    <td class="td">€{{ number_format($car->price, 2, ',', '.') }}</td>
-                    <td class="td">{{ number_format($car->mileage, 0, ',', '.') }} km</td>
-                    <td class="td">
-                        <a href="{{ route('cars.edit', $car) }}">Bewerken</a>
-                    </td>
-                    <td class="td">
-                        <form action="{{ route('cars.destroy', $car) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="text-red-600 hover:underline">Verwijder aanbod</button>
-                        </form>
-                    </td>
-                </tr>
+
+        <div class="grid grid-cols-4 gap-4 mx-auto my-14">
+
+            @foreach ($cars as $car)
+                <div class="border border-black w-[250px] p-4 rounded-sm">
+
+                    <h2 class="text-2xl font-bold">{{ $car->brand }} </h2>
+                    <h3 class="text-lg font-semibold">{{ $car->model }}</h3>
+                    <p class="text-gray-600"><strong>Kenteken:</strong> {{ $car->license_plate }}</p>
+                    <p class="text-gray-600"><strong>Prijs:</strong> €{{ number_format($car->price, 2, ',', '.') }}</p>
+                    <p class="text-gray-600"><strong>Kilometerstand:</strong> {{ number_format($car->mileage, 0, ',', '.') }} km</p>
+                    <a href="{{ route('cars.edit', $car) }}">Bewerken</a>
+                    <form action="{{ route('cars.destroy', $car) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="text-red-600 hover:underline">Verwijder aanbod</button>
+                    </form>
+
+                </div>
             @endforeach
-        </table>
+
+        </div>
     @endif
 </x-app-layout>
